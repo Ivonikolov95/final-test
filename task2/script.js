82fa2579a1615e2b28c457048341f1ad9487d55b
Ivo <Ivonikolov95@gmail.com>
2017-01-10 00:02:19 +0200

    Update script.js

--- task2/script.js
+++ task2/script.js
@@ -3,16 +3,39 @@
 // задача 1
 
 var arr = [2, 5, 8, 4, 1, 12];
+arr.sort(function(a, b){return a-b});
 
 // задача 2
 
-var text = "The quick brown fox jumps over the lazy dog";
+var str = 'The quick brown fox jumps over the lazy dog';
+    
+str = str.split( ' ' ).filter(function ( str ) {
+    var word = str.match(/(\w+)/);
+    return word && word[0].length > 4;
+}).join( ' ' );
+
+document.write( str );
+
 
 // задача 3
+
+var myVar;
+
+function myStartFunction() {
+    myVar = setInterval(function () {
+        alertFunc("text");
+    }, 1000);
+}
+
+function alertFunc(param1) {
+    document.getElementById("demo").innerHTML += "repetition is fun ";
+}
 
 
 // задача 4
 
 
 // задача 5
+
+value_or_null = (document.cookie.match(/^(?:.*;)?sessionKey=([^;]+)(?:.*)?$/)||[,null])[1]
 
